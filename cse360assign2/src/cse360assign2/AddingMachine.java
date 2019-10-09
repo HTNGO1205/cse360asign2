4fb7f46a303b2f346bccd574dc8b38e9b63a4c65
Hieu Ngo <htngo2@asu.edu>
2019-10-09 16:45:43 -0700

    just changes

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -4,27 +4,32 @@ public class AddingMachine {
 
 	private int total;
 	
-	public AddingMachine () {
+	public AddingMachine (){
+		System.out.println("Hello world 1");
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
+		System.out.println("Hello world 2");
 		return 0;
 	}
 	
 	public void add (int value) {
+		System.out.println("Hello world 3");
 		
 	}
 	
 	public void subtract (int value) {
-		
+		System.out.println("Hello world 4");
 	}
 		
 	public String toString () {
+		System.out.println("Hello world 5");
 		return "";
 	}
 
 	public void clear() {
+		System.out.println("Hello world 6");
 	
 	}
 }
