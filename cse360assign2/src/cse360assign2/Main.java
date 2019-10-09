68471a321e7a841b4b17edb886fca89aad834933
Hieu Ngo <Hieu Ngo@DESKTOP-5FL6Q1T>
2019-10-09 16:49:22 -0700

    with main

---
+++ cse360assign2/src/cse360assign2/Main.java
@@ -0,0 +1,22 @@
+package cse360assign2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		AddingMachine add = new AddingMachine();
+		int value = 0;
+		add.getTotal();
+		add.add(value);
+		add.subtract(value);
+		add.toString();
+		add.clear();
+		
+		
+		
+		
+		
+	}
+
+}
