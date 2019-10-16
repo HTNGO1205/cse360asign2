3a230d49799bec00eab38b0ae0ac65853beee2d9
Hieu Ngo <Hieu Ngo@DESKTOP-5FL6Q1T>
2019-10-16 12:42:37 -0700

    cse360Addingmachine

---
+++ cse360assign3/src/cse360assign3/AddingMachine.java
@@ -0,0 +1,45 @@
+// Hieu Ngo
+// ASU ID: 1207756000
+// basic calculator for CSE 360 class
+
+
+//https://github.com/HTNGO1205/cse360asign2
+
+package cse360assign3;
+
+public class AddingMachine {
+	private String print1 = "0 ";
+	private int total;
+	
+	public AddingMachine (){
+		
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return total;
+	}
+	
+	public void add (int value) {
+		total = total + value;
+		print1 = print1 + " + " + value;
+	}
+	
+	public void subtract (int value) {
+		total = total - value;
+		print1 = print1 +" - "+value;
+
+	}
+		
+	public String toString () {
+
+		return print1 + " ";
+	}
+
+	public void clear() {
+		
+		total = 0;
+		print1="0";
+	
+	}
+}
---
+++ cse360assign3/src/cse360assign3/Main.java
@@ -0,0 +1,36 @@
+// Hieu Ngo
+// ASU ID: 1207756000
+// basic calculator for CSE 360 class
+
+
+//https://github.com/HTNGO1205/cse360asign2
+
+
+package cse360assign3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		AddingMachine cal = new AddingMachine();
+		
+		cal.add(4);
+		cal.subtract(2);
+		cal.add(5);
+		cal.getTotal();
+		System.out.print(cal.toString());
+		System.out.print("= "+cal.getTotal()+"\n");
+
+		
+		cal.clear();
+		cal.add(5);
+		cal.subtract(2);
+		cal.add(3);
+		System.out.print(cal.toString());
+		System.out.print("= "+cal.getTotal()+"\n");
+
+		
+		
+	}
+
+}
